d2e0d4eb7b3c0db9f6a4f0e2d44cdd100149e308
mo7amednasr72 <mohamednasr7222@gmail.com>
2025-04-04 07:38:13 +0200

    fix: layout navbar in login view and register view

--- MVC_Day07/Lab3/Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ MVC_Day07/Lab3/Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1383e9b327532af3ac35696380c4f9f47cf744c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
